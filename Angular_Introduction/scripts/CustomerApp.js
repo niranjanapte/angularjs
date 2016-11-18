1486102cdb590b7c80a371cfb610174821aa57e8
niranjan apte <niranjan.apte@gmail.com>
2016-11-18 16:13:14 +0530

    Add files via upload

---
+++ Angular_Introduction/scripts/CustomerApp.js
@@ -0,0 +1,21 @@
+﻿
+
+angular.module('app', [])
+.controller('Cust1Controller', ['$scope', function ($scope) {
+    $scope.customer = {
+        name: 'amit',
+        address: 'sinhagad road'
+    };
+}])
+.controller('Cust2Controller', ['$scope', function ($scope) {
+    $scope.customer = {
+        name: 'suresh',
+        address: 'kothrud'
+    };
+}])
+.directive('myCustomer', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'my-customer.html'
+    };
+});
